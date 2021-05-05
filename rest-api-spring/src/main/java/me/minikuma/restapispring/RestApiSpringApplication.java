8880a114c0ca13870dfe761985b2a62639439529
minikuma <minikuma@naver.com>
2021-05-06 00:40:11 +0900

    initial commit

---
+++ rest-api-spring/src/main/java/me/minikuma/restapispring/RestApiSpringApplication.java
@@ -0,0 +1,11 @@
+package me.minikuma.restapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiSpringApplication.class, args);
+    }
+}
---
+++ rest-api-spring/src/test/java/me/minikuma/restapispring/RestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minikuma.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
