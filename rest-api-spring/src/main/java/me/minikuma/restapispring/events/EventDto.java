cb761dcecdd7ecd70b60c911ef8a915ddec7309f
minikuma <minikuma@naver.com>
2021-05-12 00:11:27 +0900

    POST /api/events input validation bad request check

--- rest-api-spring/src/main/java/me/minikuma/restapispring/events/EventDto.java
+++ rest-api-spring/src/main/java/me/minikuma/restapispring/events/EventDto.java
@@ -5,18 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class EventDto {
+
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
-    private String location; // 미 존재하면 온라인 모임으로 세팅됨
-    private int basePrice;
-    private int maxPrice;
+    private String location; // (Optional) 미 존재하면 온라인 모임으로 세팅됨
+    @Min(0)
+    private int basePrice; // (Optional)
+    @Min(0)
+    private int maxPrice;  // (Optional)
+    @Min(0)
     private int limitEnrollment;
 }
--- rest-api-spring/src/test/java/me/minikuma/restapispring/events/EventControllerTest.java
+++ rest-api-spring/src/test/java/me/minikuma/restapispring/events/EventControllerTest.java
@@ -1,17 +1,12 @@
 package me.minikuma.restapispring.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.aspectj.lang.annotation.Before;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -47,7 +42,36 @@ public class EventControllerTest {
     }
 
     @Test
-    public void 이벤트를만든다() throws Exception {
+    public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Java Study")
+                .description("Java REST API Study")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 5, 10, 0, 0, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021, 5,11, 0, 0, 0))
+                .beginEventDateTime(LocalDateTime.of(2021, 5, 10, 0, 0, 0))
+                .endEventDateTime(LocalDateTime.of(2021, 5, 10, 0, 0, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitEnrollment(100)
+                .location("강남역 D2 StartUp Factory")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json;charset=UTF-8"))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Java Study")
@@ -66,16 +90,19 @@ public class EventControllerTest {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json;charset=UTF-8"))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+                .andExpect(status().isBadRequest());
     }
 }
