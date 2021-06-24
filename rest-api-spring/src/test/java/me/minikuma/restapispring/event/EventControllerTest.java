eedf981c8743aea3c50773a8b8f55499be6e7807
minikuma <minikuma@naver.com>
2021-06-25 00:26:40 +0900

    customized Errors Json Body Setting

--- rest-api-spring/src/test/java/me/minikuma/restapispring/event/EventControllerTest.java
+++ rest-api-spring/src/test/java/me/minikuma/restapispring/event/EventControllerTest.java
@@ -1,6 +1,7 @@
 package me.minikuma.restapispring.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.minikuma.restapispring.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +32,7 @@ public class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성한다.")
     public void createEvent() throws Exception {
         // Post Request Body
         EventDto event = EventDto.builder()
@@ -63,6 +64,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 입력하는 경우에 에러(Bad_Request)가 발생한다.")
     public void createEvent_Bad_Request() throws Exception {
         // Post Request Body
         Event event = Event.builder()
@@ -93,6 +95,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력값이 아무것도 입력되지 않은 경우에 에러(Bad_Request)가 발생한다.")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto event = EventDto.builder().build();
 
@@ -105,6 +108,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력된 값이 잘못된 경우에 에러가 발생한다.")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         // Post Request Body
         EventDto event = EventDto.builder()
@@ -123,8 +127,12 @@ public class EventControllerTest {
         this.mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(this.objectMapper.writeValueAsString(event))
-        )
+                )
                 .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists())
         ;
     }
 }
