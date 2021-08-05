d70aa556b47392203490f61e3288e87abb183f5c
minikuma <minikuma@naver.com>
2021-08-05 14:35:58 +0900

    initial commit

---
+++ spring-mvc-1/src/main/java/hello/servlet/SevletApplication.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SevletApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SevletApplication.class, args);
+    }
+}
