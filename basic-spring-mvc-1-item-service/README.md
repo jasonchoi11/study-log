52ba47dc16d7c13ccdaf1c72c1fbd3d296cf9ffa
minikuma <minikuma@naver.com>
2021-09-27 23:35:51 +0900

    spring mvc vol.2 input form

--- basic-spring-mvc-1-item-service/README.md
+++ basic-spring-mvc-1-item-service/README.md
@@ -7,5 +7,18 @@
 * Spring Boot 2.5.4
 * JDK 11    
 
+---    
+
+#### Feature    
+
+* Item Service - Thymeleaf 기능 구현
+* Item Service - Spring 통합 기능 구현
+  * Thymeleaf-Spring 설정 적용 (```spring-boot-starter-thymeleaf``` 자동구성 지원)
+  * 입력 폼 처리 (```th:object```, ```th:field```, ```*{...}```)
+  * 체크박스
+  * 라디오 버튼
+  * 셀렉트 박스
+
 ---
+
 
--- basic-spring-mvc-1-item-service/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ basic-spring-mvc-1-item-service/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,4 +1,4 @@
-package hello.itemservice.web.basic;
+package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -8,13 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
-@RequestMapping("/basic/items")
+@RequestMapping("/form/items")
 @RequiredArgsConstructor
-public class BasicItemController {
+public class FormItemController {
 
     private final ItemRepository itemRepository;
 
@@ -22,22 +21,23 @@ public class BasicItemController {
     public String items(Model model) {
         List<Item> findItems = itemRepository.findAll();
         model.addAttribute("items", findItems);
-        return "basic/items";
+        return "form/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item findItem = itemRepository.findById(itemId);
         model.addAttribute("item", findItem);
-        return "basic/item";
+        return "form/item";
     }
 
     @GetMapping("/add")
-    public String addForm() {
-        return "basic/addForm";
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "form/addForm";
     }
 
-//    @PostMapping("/add")
+    //    @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
                             @RequestParam int price,
                             @RequestParam int quantity,
@@ -50,7 +50,7 @@ public class BasicItemController {
         itemRepository.save(item);
         model.addAttribute("item", item);
 
-        return "basic/item";
+        return "form/item";
     }
 
     /**
@@ -63,18 +63,18 @@ public class BasicItemController {
     public String addItemV2(@ModelAttribute(name = "item") Item item, Model model) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // 자동 추가, 생략 가능
-        return "basic/item";
+        return "form/item";
     }
 
     /**
-     *   @ModelAttribute name 도 생략 가능
-     *   클래스 이름을 따라간다.
+     * @ModelAttribute name 도 생략 가능
+     * 클래스 이름을 따라간다.
      */
 //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, Model model) {
         itemRepository.save(item);
 //        model.addAttribute("item", item); // 자동 추가, 생략 가능
-        return "basic/item";
+        return "form/item";
     }
 
     /**
@@ -83,48 +83,39 @@ public class BasicItemController {
 //    @PostMapping("/add")
     public String addItemV4(Item item, Model model) {
         itemRepository.save(item);
-        return "basic/item";
+        return "form/item";
     }
 
     /**
-     *  Post/Redirect/Get 적용
+     * Post/Redirect/Get 적용
      */
 //    @PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/form/items/" + item.getId();
     }
 
     /**
-     *  Post/Redirect/Get -> "저장되었습니다" 알람을 띄우고 싶을 때
+     * Post/Redirect/Get -> "저장되었습니다" 알람을 띄우고 싶을 때
      */
     @PostMapping("/add")
     public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
         Item saveItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", saveItem.getId());
         redirectAttributes.addAttribute("status", true); // query param
-        return "redirect:/basic/items/{itemId}";
+        return "redirect:/form/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item findItem = itemRepository.findById(itemId);
         model.addAttribute("item", findItem);
-        return "basic/editForm";
+        return "form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
-        return "redirect:/basic/items/{itemId}";
-    }
-
-    /**
-     * 전체조회(find all) 검증을 위한 데이터 적재
-     */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 1000, 2));
-        itemRepository.save(new Item("itemB", 10000, 20));
+        return "redirect:/form/items/{itemId}";
     }
 }
